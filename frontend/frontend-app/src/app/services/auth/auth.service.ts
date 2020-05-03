cdf9a2babb22f7cdb5e3c5f0e86b8c9d666364da
Stan <stanislaw@ulam.io>
2020-05-03 15:15:42 +0200

    frontend clean up - get rid of old auththentication component and replace it with the SPA one build upon observables objects

--- frontend/frontend-app/src/app/services/auth/auth.service.ts
+++ frontend/frontend-app/src/app/services/auth/auth.service.ts
@@ -5,9 +5,6 @@ import Auth0Client from '@auth0/auth0-spa-js/dist/typings/Auth0Client';
 import { from, of, Observable, BehaviorSubject, combineLatest, throwError } from 'rxjs';
 import { tap, catchError, concatMap, shareReplay } from 'rxjs/operators';
 
-import * as auth0 from 'auth0-js';
-import * as moment from 'moment';
-
 import { environment } from '../../../environments/environment';
 
 
@@ -112,76 +109,4 @@ export class AuthService {
       concatMap((client: Auth0Client) => from(client.getTokenSilently(options)))
     );
   }
-
-  // auth0 = new auth0.WebAuth({
-  //   clientID: environment.auth.clientID,
-  //   domain: environment.auth.domain,
-  //   responseType: 'token',
-  //   redirectUri: environment.auth.auth0RedirectUri,
-  //   audience: environment.auth.audience,
-  //   scope: 'openid profile'
-  // });
-
-  // constructor(
-  //   public router: Router
-  // ) { }
-
-  // public login() {
-  //   this.auth0.authorize();
-  // }
-
-  // public handleLoginCallback(): void {
-  //   this.auth0.parseHash((err, authResult) => {
-  //     if (authResult && authResult.accessToken) {
-  //       window.location.hash = '';
-  //       this.getUserInfo(authResult);
-  //     } else if (err) {
-  //       console.log(`Error: ${err.error}`);
-  //     }
-  //     this.router.navigate(['/']);
-  //   });
-  // }
-
-  // public getAccessToken() {
-  //   return localStorage.getItem("token");
-  // }
-
-  // public getUserInfo(authResult) {
-  //   this.auth0.client.userInfo(authResult.accessToken, (err, profile) => {
-  //     if (profile) {
-  //       this.setSession(authResult, profile);
-  //     }
-  //   });
-  // }
-
-  // private setSession(authResult, profile): void {
-  //   const expiresAt = moment().add(authResult.expiresIn, 'seconds');
-
-  //   localStorage.setItem("token", authResult.accessToken);
-  //   localStorage.setItem("expiresAt", JSON.stringify(expiresAt.valueOf()));
-  // }
-
-  // public logout(): void {
-  //   localStorage.removeItem("token");
-  //   localStorage.removeItem("expiresAt");
-  //   this.auth0.logout({
-  //     returnTo: environment.auth.auth0ReturnTo,
-  //     clientId: environment.auth.clientID
-  //   });
-  // }
-
-  // get isLoggedIn(): boolean {
-  //   // return Date.now() < this.expiresAt && this.authenticated;
-  //   return moment().isBefore(this.getExpiration()) && this.isTokenSet();
-  // }
-
-  // isTokenSet(): boolean {
-  //   const token = this.getAccessToken();    
-  //   return token !== undefined && token !== null && token !== '';
-  // }
-
-  // getExpiration() { // TODO
-  //   const expiration = localStorage.getItem("expiresAt");
-  //   return moment(JSON.parse(expiration));
-  // }
 }
